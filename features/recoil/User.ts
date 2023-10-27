77202cf4cf5cc899249c7772ebd0157ffa8cceb5
Theodoros Solomou <solomou.theodoros@gmail.com>
2023-10-27 11:18:49 +0300

    Resolved a merge conflict

--- features/recoil/User.ts
+++ features/recoil/User.ts
@@ -2,27 +2,7 @@ import {atom} from 'recoil';
 import {IUser} from './interfaces/IUser';
 import {User} from './models/User';
 
-<<<<<<< HEAD
-export interface User {
-  id: string;
-  name: string;
-  surname: string;
-  loggedIn: boolean;
-  token: string;
-}
-
-export const userState = atom<User>({
-  key: 'userState',
-  default: {
-    id: '',
-    name: '',
-    surname: '',
-    loggedIn: false,
-    token: '',
-  },
-=======
 export const userState = atom<IUser>({
   key: 'userState',
   default: new User(),
->>>>>>> a5ebdff6897328e616c660c74a1495122a9eba3e
 });
