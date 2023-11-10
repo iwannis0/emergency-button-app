bd5d1c3eee38a3195bfb0727a89b56bd45a0ec91
Theodoros Solomou <solomou.theodoros@gmail.com>
2023-11-10 15:50:37 +0200

    revert test

--- screens/SettingsScreen/style.js
+++ screens/SettingsScreen/style.js
@@ -7,7 +7,7 @@ const myHealthDashboardStyle = StyleSheet.create({
     backgroundColor: '#ffffff',
   },
   modalInformation: {
-    marginLeft: horizontalScale(21),
+    marginLeft: horizontalScale(20),
     marginRight: horizontalScale(30),
   },
 });
