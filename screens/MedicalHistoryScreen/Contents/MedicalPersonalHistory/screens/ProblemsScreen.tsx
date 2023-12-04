2daa4b06f57a8f2ae3bd56667fd892bd98d67c2c
Theodoros Solomou <solomou.theodoros@gmail.com>
2023-12-04 11:50:40 +0200

    Minor commit

--- screens/MedicalHistoryScreen/Contents/MedicalPersonalHistory/screens/ProblemsScreen.tsx
+++ screens/MedicalHistoryScreen/Contents/MedicalPersonalHistory/screens/ProblemsScreen.tsx
@@ -5,7 +5,6 @@ import globalStyle from '../../../../../assets/styles/globalStyle';
 import CurrentProblems from '../CurrentProblems';
 import ResolvedProblems from '../ResolvedProblems';
 import Procedures from '../Procedures';
-import FunctionalStatus from '../FunctionalStatus';
 
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 import {verticalScale} from '../../../../../assets/styles/scaling';
